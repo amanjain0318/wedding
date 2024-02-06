cc7838211bdc567d6f6b924196b75d746eb1ba89
Aman Jain <amanj0318@gmail.com>
2024-02-06 10:41:21 +0530

    First Commit

---
+++ WeddingPlanning/src/test/java/com/wedding/WeddingPlanning/WeddingPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedding.WeddingPlanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingPlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
